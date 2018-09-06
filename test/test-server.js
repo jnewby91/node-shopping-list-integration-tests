42257fe7ba3743564525ff78ce0b177d9e646bf5
Justin Newby <jnewby91@gmail.com>
2018-09-06 17:55:09 -0400

    deletes unnecessary Mocha code

--- test/test-server.js
+++ test/test-server.js
@@ -58,9 +58,6 @@ describe("Recipes", function(){
       })
       .then(function(res){
         expect(res).to.have.status(204);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
       });
   });
   it("should delete recipe items on DELETE", function(){
